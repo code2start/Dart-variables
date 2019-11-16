52903fffbb1ed36a1df1d2bc41cc5244a0d74f77
Mohamed Fathy <codetwostart@gmail.com>
2019-11-16 13:40:23 +0200

    Initial commit

---
+++ main.dart
@@ -0,0 +1,14 @@
+main() {
+  int x; //variable declaration
+  x = 5; //variable initialzation
+  print("x = " + x.toString()); //concatination
+  String name = "Mohamed Fathy";
+  //print("Your name is " + name);//concatination
+  print("Your name is $name"); //string interpolation
+  var age = 5; //dart is strongly typed
+  age = 44;
+  var y;
+  y = 22;
+  y = "Khaled";
+  print("Name is $y");
+}
